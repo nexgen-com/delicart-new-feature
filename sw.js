3f2f4decfa4f66cafe293995ad0fa3b1fbf9ac4b
phamkykhoi <phamkykhoi.info@gmail.com>
2025-11-04 21:25:37 +0700

    Apply mobile

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'delicart-lunch-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/login.html',
+  '/admin-dashboard.html',
+  '/shop-dashboard.html'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate Event
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
